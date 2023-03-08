3535b931ce4ca788a4b6177d93d866a1ec7888b1
gurkenpoo <bastianpoo@gmail.com>
2023-03-07 22:26:12 -0300

    add player health bar

--- player.gd
+++ player.gd
@@ -1,5 +1,7 @@
 extends CharacterBody3D
 
+signal health_changed(health_value)
+
 @onready var camera = $Camera3D
 @onready var anim_player = $AnimationPlayer
 @onready var fuegoDisparo = $Camera3D/pistol/fuego
@@ -81,6 +83,7 @@ func recieve_damage():
 	if health <= 0:
 		health = 3
 		position = Vector3.ZERO
+	health_changed.emit(health)
 
 func _on_animation_player_animation_finished(anim_name):
 	if anim_name == "shoot":
--- world.gd
+++ world.gd
@@ -2,6 +2,8 @@ extends Node
 
 @onready var mainMenu = $CanvasLayer/mainMenu
 @onready var entry = $CanvasLayer/mainMenu/MarginContainer/VBoxContainer/addressentry
+@onready var hud = $CanvasLayer/HUD
+@onready var health_bar = $CanvasLayer/HUD/healthBar
 
 const Player = preload("res://player.tscn")
 const PORT = 9999
@@ -14,19 +16,37 @@ func _unhandled_input(event):
 
 func _on_host_button_pressed():
 	mainMenu.hide()
+	hud.show()
 	
 	enet_peer.create_server(PORT)
 	multiplayer.multiplayer_peer = enet_peer
 	multiplayer.peer_connected.connect(addPlayer)
+	multiplayer.peer_disconnected.connect(remove_player)
 	
 	addPlayer(multiplayer.get_unique_id())
 
 func _on_join_button_pressed():
 	mainMenu.hide()
-	
+	hud.show()
 	enet_peer.create_client("localhost", PORT)
 	multiplayer.multiplayer_peer = enet_peer
+	
 func addPlayer(peer_id):
 	var player = Player.instantiate()
 	player.name = str(peer_id)
 	add_child(player)
+	if player.is_multiplayer_authority():
+		player.health_changed.connect(update_health_bar)
+		
+func remove_player(peer_id):
+	var player = get_node_or_null(str(peer_id))
+	if player:
+		player.queue_free()
+		
+func update_health_bar(health_value):
+	health_bar.value = health_value
+
+
+func _on_multiplayer_spawner_spawned(node):
+	if node.is_multiplayer_authority():
+		node.health_changed.connect(update_health_bar)
